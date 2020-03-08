6b65ecca7e895187824c71dd47c2293f87f329ff
SCscHero <39325371+SCscHero@users.noreply.github.com>
2020-03-08 17:21:17 +0800

    Update OperatorFactory.cs
    增加注释

--- CalcDemn/OperatorFactory.cs
+++ CalcDemn/OperatorFactory.cs
@@ -14,7 +14,9 @@ namespace CalculateDemo
         {
             Operator opr = null;
             Type cls = Type.GetType(name);
+            //GetConstructor(), GetConstructors()：返回ConstructorInfo类型，用于取得该类的构造函数的信息
             ConstructorInfo ci = cls.GetConstructor(Type.EmptyTypes);
+            //调用具有指定参数的实例反映的构造函数，为不常用的参数提供默认值
             opr = (Operator)ci.Invoke(null);
             return opr;
         }
